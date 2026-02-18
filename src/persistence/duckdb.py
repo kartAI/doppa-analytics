4bc3c8e5f74550ce0b02d516e2eb01bb67c864e8
Jathavaan Shankarr <jathavaan12@gmail.com>
2026-02-18 14:16:42 +0100

    #1 Created notebook that visualized simple monitor logs

---
+++ src/persistence/duckdb.py
@@ -0,0 +1,24 @@
+﻿import platform
+
+import duckdb
+
+
+def create_duckdb_context() -> duckdb.DuckDBPyConnection:
+    db_context: duckdb.DuckDBPyConnection = duckdb.connect()
+    db_context.install_extension("spatial")
+    db_context.load_extension("spatial")
+    db_context.install_extension("azure")
+    db_context.load_extension("azure")
+
+    db_context.execute("""
+    CREATE OR REPLACE SECRET azure_secret(
+        TYPE azure,
+        PROVIDER config,
+        ACCOUNT_NAME 'doppablobstorage'
+    );
+    """)
+
+    if platform.system() == "Linux":
+        db_context.execute("SET azure_transport_option_type = curl")
+
+    return db_context
