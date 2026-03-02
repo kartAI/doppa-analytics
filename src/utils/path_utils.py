4b63bda758174b190a02c4c3384686a9baf2c393
Jathavaan Shankarr <jathavaan12@gmail.com>
2026-03-02 16:36:40 +0100

    #11 Added notebok for network analytics

--- src/utils/path_utils.py
+++ src/utils/path_utils.py
@@ -2,10 +2,14 @@
         container: str,
         query_id: str,
         run_id: str,
+        benchmark_run: int,
         iteration: int,
         file_name: str
 ) -> str:
+    if benchmark_run == -1:
+        benchmark_run = "*"
+
     if iteration == -1:
         iteration = "*"
 
-    return f"az://{container}/query_id={query_id}/run_id={run_id}/iteration={iteration}/{file_name}"
+    return f"az://{container}/query_id={query_id}/run_id={run_id}/benchmark_run={benchmark_run}/iteration={iteration}/{file_name}"
